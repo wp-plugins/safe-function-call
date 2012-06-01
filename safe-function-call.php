e3e3f687a58edc1499cf9d6ec3c53998cba2bfed
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-01 03:33:38 +0000

    * Update to v1.1.6 * Re-license as GPLv2 or later (from X11) * Add 'License' and 'License URI' header tags to readme.txt and plugin file * Remove ending PHP close tag * Miscellaneous readme.txt changes * Update copyright date (2012) * Note compatibility through WP 3.4+
    git-svn-id: https://plugins.svn.wordpress.org/safe-function-call/trunk@551629 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- safe-function-call.php
+++ safe-function-call.php
@@ -2,17 +2,19 @@
 /**
  * @package Safe_Function_Call
  * @author Scott Reilly
- * @version 1.1.5
+ * @version 1.1.6
  */
 /*
 Plugin Name: Safe Function Call
-Version: 1.1.5
+Version: 1.1.6
 Plugin URI: http://coffee2code.com/wp-plugins/safe-function-call/
 Author: Scott Reilly
-Author URI: http://coffee2code.com
+Author URI: http://coffee2code.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Description: Safely and easily call functions that may not be available (such as those provided by a plugin that gets deactivated).
 
-Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+, 3.2+, 3.3+.
+Compatible with WordPress 1.5 through 3.4+.
 
 =>> Read the accompanying readme.txt file for instructions and documentation.
 =>> Also, visit the plugin's homepage for additional information and updates.
@@ -21,114 +23,114 @@ Compatible with WordPress 1.5+, 2.0+, 2.1+, 2.2+, 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 
 */
 
 /*
-Copyright (c) 2007-2011 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2007-2012 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 if ( ! function_exists( '_sfc' ) ) :
-/**
- * Safely invoke the function by the name of $function_name.  Any additional
- * arguments will get passed to $function_name().  If $function_name() does
- * not exist, nothing is displayed and no error is generated.
- *
- * @param string $function_name Name of the function to call
- * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
- */
-function _sfc( $function_name ) {
-	if ( ! function_exists( $function_name ) )
-		return;
-	$args = array_slice( func_get_args(), 1 );
-	return call_user_func_array( $function_name, $args );
-}
+	/**
+	 * Safely invoke the function by the name of $function_name.  Any additional
+	 * arguments will get passed to $function_name().  If $function_name() does
+	 * not exist, nothing is displayed and no error is generated.
+	 *
+	 * @param string $function_name Name of the function to call
+	 * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
+	 */
+	function _sfc( $function_name ) {
+		if ( ! function_exists( $function_name ) )
+			return;
+		$args = array_slice( func_get_args(), 1 );
+		return call_user_func_array( $function_name, $args );
+	}
 endif;
 
 
 if ( ! function_exists( '_sfce' ) ) :
-/**
- * Safely invoke the function by the name of $function_name and echo its return
- * value .  Any additional arguments will get passed to $function_name().  If
- * $function_name() does not exist, nothing is displayed and no error is
- * generated.
- *
- * This functions is the same as _sfc() except that it echoes the return value
- * of $function_name() before returning that value.
- *
- * @param string $function_name Name of the function to call
- * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
- */
-function _sfce( $function_name ) {
-	if ( ! function_exists( $function_name ) )
-		return;
-	$args = func_get_args();
-	$value = call_user_func_array( '_sfc', $args );
-	if ( $value )
-		echo $value;
-	return $value;
-}
+	/**
+	 * Safely invoke the function by the name of $function_name and echo its return
+	 * value .  Any additional arguments will get passed to $function_name().  If
+	 * $function_name() does not exist, nothing is displayed and no error is
+	 * generated.
+	 *
+	 * This functions is the same as _sfc() except that it echoes the return value
+	 * of $function_name() before returning that value.
+	 *
+	 * @param string $function_name Name of the function to call
+	 * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
+	 */
+	function _sfce( $function_name ) {
+		if ( ! function_exists( $function_name ) )
+			return;
+		$args = func_get_args();
+		$value = call_user_func_array( '_sfc', $args );
+		if ( $value )
+			echo $value;
+		return $value;
+	}
 endif;
 
 
 if ( ! function_exists( '_sfcf' ) ) :
-/**
- * Safely invoke the function by the name of $function_name.  Any additional
- * arguments will get passed to $function_name().  If $function_name() does
- * not exist, then an alternative function is called.
- *
- * This functions is the same as _sfc() except that if the intended function
- * does not exist, then an alternative function is invoked (if it exists
- * itself).
- *
- * @param string $function_name Name of the function to call
- * @param string $function_if_missing (optional) Name of the alternative function to call if $function_name does not exists
- * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
- */
-function _sfcf( $function_name, $function_if_missing = null ) {
-	$args = array_slice( func_get_args(), 2 );
-	if ( ! function_exists( $function_name ) ) {
-		if ( function_exists( $function_if_missing ) ) {
-			$args = array_merge( array( $function_name ), $args );
-			return call_user_func_array( $function_if_missing, $args );
+	/**
+	 * Safely invoke the function by the name of $function_name.  Any additional
+	 * arguments will get passed to $function_name().  If $function_name() does
+	 * not exist, then an alternative function is called.
+	 *
+	 * This functions is the same as _sfc() except that if the intended function
+	 * does not exist, then an alternative function is invoked (if it exists
+	 * itself).
+	 *
+	 * @param string $function_name Name of the function to call
+	 * @param string $function_if_missing (optional) Name of the alternative function to call if $function_name does not exists
+	 * @return mixed If $function_name exists as a function, returns whatever that function returns. Otherwise, returns nothing.
+	 */
+	function _sfcf( $function_name, $function_if_missing = null ) {
+		$args = array_slice( func_get_args(), 2 );
+		if ( ! function_exists( $function_name ) ) {
+			if ( function_exists( $function_if_missing ) ) {
+				$args = array_merge( array( $function_name ), $args );
+				return call_user_func_array( $function_if_missing, $args );
+			}
+			return;
 		}
-		return;
+		return call_user_func_array( $function_name, $args );
 	}
-	return call_user_func_array( $function_name, $args );
-}
 endif;
 
 
 if ( ! function_exists( '_sfcm' ) ) :
-/**
- * Safely invoke the function by the name of $function_name.  Any additional
- * arguments will get passed to $function_name().  If $function_name() does
- * not exist, then an alternative function is called with identical arguments.
- *
- * This functions is the same as _sfc() except that if the intended function
- * does not exist, then a string is echoed (if provided).
- *
- * @param string $function_name Name of the function to call
- * @param string $msg_if_missing (optional) String to be echoed if $function_name does not exist
- * @return mixed If $function_name exists as a function, returns whatever that function returns.  Otherwise, returns nothing.
- */
-function _sfcm( $function_name, $msg_if_missing = '' ) {
-	if ( ! function_exists( $function_name ) ) {
-		if ( $msg_if_missing )
-			echo $msg_if_missing;
-		return;
+	/**
+	 * Safely invoke the function by the name of $function_name.  Any additional
+	 * arguments will get passed to $function_name().  If $function_name() does
+	 * not exist, then an alternative function is called with identical arguments.
+	 *
+	 * This functions is the same as _sfc() except that if the intended function
+	 * does not exist, then a string is echoed (if provided).
+	 *
+	 * @param string $function_name Name of the function to call
+	 * @param string $msg_if_missing (optional) String to be echoed if $function_name does not exist
+	 * @return mixed If $function_name exists as a function, returns whatever that function returns.  Otherwise, returns nothing.
+	 */
+	function _sfcm( $function_name, $msg_if_missing = '' ) {
+		if ( ! function_exists( $function_name ) ) {
+			if ( $msg_if_missing )
+				echo $msg_if_missing;
+			return;
+		}
+		$args = array_slice( func_get_args(), 2 );
+		return call_user_func_array( $function_name, $args );
 	}
-	$args = array_slice( func_get_args(), 2 );
-	return call_user_func_array( $function_name, $args );
-}
 endif;
-
-?>
